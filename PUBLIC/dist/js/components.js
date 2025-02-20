71b14291c30400c2632ebf5ca7a215169344d1f9
oppaho03 <oppaho03@gmail.com>
2025-02-20 11:07:58 +0900

    add prompt

--- PUBLIC/dist/js/components.js
+++ PUBLIC/dist/js/components.js
@@ -53,7 +53,268 @@
     }
 
   } );
-  
+
+  /* 바인드 : 'prompt' (form) 초기화 
+  */ 
+  App.prototype.binds.push( function( e ) {
+    // 프롬프트 (prompt) : 폼 (Form)
+    var prompt = document.getElementById("chat-prompt");
+    if ( ! prompt ) return;
+    setEventListener( prompt, 'submit', function(e) {
+
+      e.preventDefault(); // submit 취소
+
+      var prompt = e.target;
+      var prompt_input = prompt.querySelector("input[name='s']");
+      var prompt_value = prompt_input ? prompt_input.value.trim() : "";
+      
+      if ( !prompt_value || prompt_value == "" ) {
+        if ( prompt_input ) 
+          prompt_input.focus();
+
+        return; // end submit
+      }
+
+      /* 대화 시작 : 질문 (Question) / 답변 (Answer)
+      */  
+      var cs = document.getElementById("chat-session");
+      var cc = cs ? cs.querySelector("#chat-content") : null; 
+      
+
+      if ( ! cc ) return;
+
+      var temp = cc.querySelector("#chat-template");
+      if ( temp ) {
+        
+        // document.importNode(template.content, true)
+        var content = temp.content.cloneNode(true);
+        content = content.querySelectorAll(".chat-wrap"); // [ 0: 봇 , 1: 사용자 ]
+
+        if ( ! content.length ) return;
+
+        // 아이템 생성 (chat-list-item)
+        var item = document.createElement('div');
+        item.classList = "chat-list-item";
+        for ( var el of content  ) { 
+          item.appendChild( el ); // .chat 엘리멘트 추가
+        }
+
+        var fragmentItem = document.createDocumentFragment()
+        fragmentItem.append(item);
+
+        // 채팅 리스트 (Chat List)
+        var list = cc.querySelector(".chat-list");
+        if ( list ) {
+
+          list.appendChild( fragmentItem ); 
+         
+          var items = list.children;
+          var item = Array.from(items).slice(-1)[0]; // 마지막에 추가된 아이템 선택 
+          
+          /* 대화 입력 : 질문 (Question)
+          */ 
+          var cuser = item.querySelector(".chat.type-user");
+          if ( cuser ) {
+            var cuser_cc = cuser.querySelector(".chat-content"); 
+            var cuser_cc_msgwrap = cuser_cc.querySelector(".messages");
+
+            var _msg = document.createElement("p");
+            _msg.setAttribute( "aria-label", "message" );
+            _msg.innerText = prompt_value;
+
+            cuser_cc_msgwrap.appendChild(_msg); /// append message
+          } // cuser
+        }
+    
+        prompt_input.value = ""; /// 초기화
+      }
+      else return;
+
+      /* 대화 입력 : 답변 (Question)
+      */
+      // prompt_value
+      setTimeout( function( prompt_value ) {
+
+        /// Sample data 
+        console.log( "request", prompt_value );
+
+        var data = {
+          "id" : 7,
+          "fileName": "HC-A-02781336",
+          "disease_category": "귀코목질환",
+          "disease_name": {
+              "kor": "급성 중이염",
+              "eng": "Acute otitis media"
+          },
+          "department": [
+              "이비인후과"
+          ],
+          "intention": "약물",
+          "answer": {
+              "intro": "급성 중이염의 치료는 대부분 약물을 사용하여 이루어지며, 항생제와 진통제의 사용이 필요합니다.",
+              "body": "일반적으로 의사는 급성 중이염의 치료를 위해 항생제를 처방할 것입니다. 항생제는 세균 감염에 의한 경우 가장 효과적인 약물 중 하나입니다. 항생제 치료는 의사의 처방을 받고 정해진 기간 동안 지속되어야 합니다. 또한, 귀 통증을 완화하기 위해 아세트아미노펜 또는 이부프로펜과 같은 진통제를 사용할 수 있습니다. 중이 내 분비물을 관리하기 위해 이어 플러그를 사용하는 것도 중요한 치료 방법입니다.",
+              "conclusion": "만약 환자의 증상이 호전되지 않거나 악화된다면 의사와 상담하여 추가적인 치료 옵션을 고려해야 합니다. 의사는 환자의 개별적인 상황을 평가하고 적절한 치료 계획을 수립할 것입니다."
+          },
+          "num_of_words": 88
+        };
+
+        var cs = document.getElementById("chat-session");
+        var cc = cs ? cs.querySelector("#chat-content") : null; 
+        
+        var t = cc ? cc.querySelector(".chat.type-bot[data-status='pending']") : null; /// 'pending' 상태 채팅 선택
+        if ( ! t ) return;
+
+        // 데이터 파싱 
+        t.dataset['id'] = data.id;
+        t.dataset['fileName'] = data.fileName ?? '';
+
+        var data_answer = { 
+          ...{ "intro" : "", "body" : "", "conclusion" : "" }, 
+          ...( data.hasOwnProperty("answer") ? data.answer : {} ) 
+        };
+        
+        /// 답변 : type-bubble
+        var msgsbub = t.querySelector(".messages.type-bubble");
+        if ( msgsbub ) {
+          if ( ! isUndefined( msgsbub.replaceChildren ) ) msgsbub.replaceChildren();
+          else msgsbub.innerHTML = "";
+
+          var _val = data_answer.intro; /// 답변 'intro'
+
+          if( _val.trim() ) {
+            var _msg = document.createElement("p");
+            _msg.setAttribute( "aria-label", "message" );
+            _msg.innerText = _val;
+
+            msgsbub.appendChild( _msg );
+          }
+          else msgsbub.classList.add("d-none");
+          
+        } 
+
+        /// 답변 : type-main
+        var msgs = t.querySelector(".messages.type-main");
+        if ( msgs ) {
+
+          var msgsf = document.createDocumentFragment(); 
+
+          // 답변 :  'body', 'conclusion'
+          Object.entries(data_answer).forEach( function([_key, _val]) {
+
+            if ( ['body', 'conclusion'].indexOf( _key ) === -1 ) return;
+
+            var _msg = document.createElement("p");
+
+            _msg.setAttribute( "aria-label", "message" );
+            _msg.innerText = _val;
+
+            msgsf.appendChild( _msg ); 
+          } );
+
+          if ( ! isUndefined( msgs.replaceChildren ) ) msgs.replaceChildren();
+          else msgs.innerHTML = "";
+
+          msgs.appendChild( msgsf );
+
+          /// 답변 :  'conclusion'
+
+        }
+
+        t.dataset['status'] = "fulfilled"; /// 상태 변경 (pending -> fulfilled)
+        
+      }, 3000 );
+
+      
+    }, {} );
+
+    // 프롬프트 (prompt) : 입력창 
+    var input = prompt.querySelector("input[name='s']");
+    setEventListener( input, 'keydown', function(e) {
+      var self = this;
+      var key = (String)(e.key || e.code).toLowerCase();
+
+      // 채팅 프롬프트 : 폼(form)
+      var form = self.closest( "#chat-prompt" );
+
+      if ( form ) {
+        var activated = form.classList.contains( "activated" );
+        if ( ! activated ) {
+          form.classList.add( "activated" );
+
+          var formwrap = form.closest(".form-wrap");
+          if ( formwrap ) formwrap.style.bottom =  (form.clientHeight / 2) + 'px';
+        }
+      }
+
+      var expts = [ "enter", "escape" ]; // 예외 키
+      if ( ! expts.includes( key ) ) return;
+
+      switch ( key ) {
+        case "enter" :
+          // pass
+          break;
+        
+        case "escape" :
+          self.value = "";
+          break;
+
+      }
+
+    }, { capture: false, passive: true } ); 
+
+    // 프롬프트 (prompt) 버튼 (submit)
+    var btn = prompt.querySelector("button[type='submit']");
+    setEventListener( btn, 'click', function(e) {
+
+      // var self = e.target; 
+      // var expanded = self.classList.toggle("expanded") ;
+
+      // // self.setAttribute( 'aria-expanded', expanded );
+
+      // /// 프롬프트 (prompt) 
+      // var form = self.closest('form');
+      // form.classList.toggle("activated") ;
+      // // if ( ! form.classList.contains("activated") ) {
+      // //   form.classList.add("activated"); // 활성화
+      // // }
+      
+      // // 채팅 컨텐츠 활성화 및 비활성화 토글 (Toggled chat content)
+      // var mcnt = document.getElementById( "content" );
+      // var ccnt = document.getElementById( "chat-content" );
+      // if ( expanded ) {
+      //   // 채팅 컨텐츠 : 활성화
+      //   var prevEl = ccnt.previousElementSibling;
+      //   if ( prevEl ) {
+      //     // console.log(prevEl);
+      //     prevEl.style.marginLeft= String(-100) + "%";
+      //   }
+
+      // }
+      // else {
+      //   // 채팅 컨텐츠 : 비활성화 
+      // }
+        
+    }, { capture: false, passive: false } );
+  });
+
+  // new Swiper('.swiper', {
+  //   slidesPerView: 2,
+  //   spaceBetween: 30,
+  //   centeredSlides: false,
+  //   loop: true,
+  //   // If we need pagination
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //     clickable: true,
+  //   },
+
+  //   // Navigation arrows
+  //   navigation: {
+  //     nextEl: '.swiper-button-next',
+  //     prevEl: '.swiper-button-prev',
+  //   },
+  // });
+
   
   var __APP = new App();
     
