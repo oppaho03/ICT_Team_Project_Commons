ccdb5983f9c5715d844ad89d5f01e76d924b8f97
oppaho03 <oppaho03@gmail.com>
2025-03-03 11:18:00 +0900

    uninstall.sql updated

---
+++ DB_SQL/oracle/SQL/uninstall.sql
@@ -0,0 +1,16 @@
+drop table app_terms;
+drop table app_keyword_counting;
+drop table app_term_meta;
+drop table app_term_category;
+drop table app_posts;
+drop table app_post_category_relationships;
+drop table app_post_meta;
+drop table app_resources_sec;
+drop table app_member;
+drop table app_member_sns_logins;
+drop table app_member_meta;
+drop table app_chat_answer;
+drop table app_chat_question;
+drop table app_anc;
+drop table app_chat_qna;
+drop table app_chat_session;
