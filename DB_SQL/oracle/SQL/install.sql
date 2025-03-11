e8816f7d28f78df1e0d6c29c9d6dad2e05449be5
oppaho03 <oppaho03@gmail.com>
2025-03-11 19:15:21 +0900

    SQL 수정

--- DB_SQL/oracle/SQL/install.sql
+++ DB_SQL/oracle/SQL/install.sql
@@ -443,6 +443,45 @@ BEGIN -- 프로그램 코딩부
     END IF;
 END;
 
+/* APP_CHAT_SESSION
+*/
+-- APP_CHAT_SESSION 테이블용 시퀀스 생성
+CREATE SEQUENCE APP_CHAT_SESSION_SEQ
+START WITH 1  -- 시작 값
+INCREMENT BY 1  -- 증가 값
+NOCACHE  -- 캐싱하지 않음 (캐싱:일정 개수의 시퀀스 값을 미리 메모리에 저장)
+NOCYCLE;  -- 최대값 도달 시 다시 1부터 시작하지 않음
+
+-- [대화 세션]
+create table APP_CHAT_SESSION(
+    id NUMBER(20,0) PRIMARY KEY, -- 시퀀스 값
+    member_id NUMBER(20,0) NOT NULL , -- 외래키
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL, -- 디폴트는 created_at
+    status NUMBER(1,0) NOT NULL,
+    count NUMBER(20,0) DEFAULT 0 NOT NULL,
+    
+     -- 외래키 제약조건
+    CONSTRAINT fk_member_id_chat_session FOREIGN KEY (member_id)  
+        REFERENCES APP_MEMBER(id)    
+        ON DELETE CASCADE 
+);
+
+-- APP_CHAT_SESSION 테이블에 대한 트리거 설정
+CREATE OR REPLACE TRIGGER APP_CHAT_SESSION_TRG
+BEFORE INSERT ON APP_CHAT_SESSION
+FOR EACH ROW -- 행 단위 트리거 - 행 단위로 변경된 횟수만큼 실행됨
+BEGIN -- 프로그램 코딩부
+    -- id컬럼(PK) 자동 증가
+    IF :NEW.id IS NULL THEN
+        :NEW.id := APP_CHAT_SESSION_SEQ.NEXTVAL;
+    END IF;
+    -- updated_at컬럼의 기본값을 created_at으로 설정
+    IF :NEW.updated_at IS NULL THEN
+        :NEW.updated_at := :NEW.created_at;
+    END IF;
+END;
+
 
 /* APP_CHAT_QNA
 */
@@ -516,46 +555,5 @@ BEGIN -- 프로그램 코딩부
     -- id컬럼(PK) 자동 증가
     IF :NEW.id IS NULL THEN
         :NEW.id := APP_KEYWORD_COUNTING_SEQ.NEXTVAL;
-    END IF;
-END;
-
-
-
-/* APP_CHAT_SESSION
-*/
--- APP_CHAT_SESSION 테이블용 시퀀스 생성
-CREATE SEQUENCE APP_CHAT_SESSION_SEQ
-START WITH 1  -- 시작 값
-INCREMENT BY 1  -- 증가 값
-NOCACHE  -- 캐싱하지 않음 (캐싱:일정 개수의 시퀀스 값을 미리 메모리에 저장)
-NOCYCLE;  -- 최대값 도달 시 다시 1부터 시작하지 않음
-
--- [대화 세션]
-create table APP_CHAT_SESSION(
-    id NUMBER(20,0) PRIMARY KEY, -- 시퀀스 값
-    member_id NUMBER(20,0) NOT NULL , -- 외래키
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL, -- 디폴트는 created_at
-    status NUMBER(1,0) NOT NULL,
-    count NUMBER(20,0) DEFAULT 0 NOT NULL,
-    
-     -- 외래키 제약조건
-    CONSTRAINT fk_member_id_chat_session FOREIGN KEY (member_id)  
-        REFERENCES APP_MEMBER(id)    
-        ON DELETE CASCADE 
-);
-
--- APP_CHAT_SESSION 테이블에 대한 트리거 설정
-CREATE OR REPLACE TRIGGER APP_CHAT_SESSION_TRG
-BEFORE INSERT ON APP_CHAT_SESSION
-FOR EACH ROW -- 행 단위 트리거 - 행 단위로 변경된 횟수만큼 실행됨
-BEGIN -- 프로그램 코딩부
-    -- id컬럼(PK) 자동 증가
-    IF :NEW.id IS NULL THEN
-        :NEW.id := APP_CHAT_SESSION_SEQ.NEXTVAL;
-    END IF;
-    -- updated_at컬럼의 기본값을 created_at으로 설정
-    IF :NEW.updated_at IS NULL THEN
-        :NEW.updated_at := :NEW.created_at;
     END IF;
 END;
